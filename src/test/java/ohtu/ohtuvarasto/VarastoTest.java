e23496cc94222d4e6fcd20843572e01c8e170337
narkusv97 <narkusv97@gmail.com>
2020-11-02 18:42:26 +0200

    Tehty testit

--- src/test/java/ohtu/ohtuvarasto/VarastoTest.java
+++ src/test/java/ohtu/ohtuvarasto/VarastoTest.java
@@ -64,5 +64,40 @@ public class VarastoTest {
         // varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         assertEquals(4, varasto.paljonkoMahtuu(), vertailuTarkkuus);
     }
+	
+	@Test
+    public void test1() {
+        varasto.otaVarastosta(12);
 
+        assertEquals(10, varasto.paljonkoMahtuu(), vertailuTarkkuus);
+    }
+	
+	@Test
+    public void test2() {
+        varasto.lisaaVarastoon(12);
+
+        assertEquals(0.0, varasto.paljonkoMahtuu(), vertailuTarkkuus);
+    }
+	
+	@Test
+    public void test3() {
+        varasto.lisaaVarastoon(18);
+
+        assertEquals(10, varasto.getSaldo(), vertailuTarkkuus);
+    }
+	
+	@Test
+    public void test4() {
+        varasto.lisaaVarastoon(-4);
+
+        assertEquals(10, varasto.paljonkoMahtuu(), vertailuTarkkuus);
+    }
+	
+	@Test
+    public void test5() {
+        varasto.otaVarastosta(18);
+
+        assertEquals(0, varasto.getSaldo(), vertailuTarkkuus);
+    }
+	
 }
